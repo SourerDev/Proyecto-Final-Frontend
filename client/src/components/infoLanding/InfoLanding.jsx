ea77701c237880f385bd4e47574db092cc864e4a
arturogutierrez11 <arturogutierrez@Arturos-MacBook-Pro.local>
2022-11-14 12:55:12 -0300

    style en cards en landing

--- client/src/components/infoLanding/InfoLanding.jsx
+++ client/src/components/infoLanding/InfoLanding.jsx
@@ -3,17 +3,17 @@ export default  function CardsLanding(){
       
         <div className="  lg:flex m-1 p-1 justify-center items-center inline">
           <a href="#" class="block p-4 max-w-sm rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 bg-gray-800  hover:bg-gray-700">
-          <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+          <svg class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
             <h5 class="mb-2 text-2xl font-bold tracking-tight  text-white">En nuestra cultura</h5>
             <p class="font-normal text-gray-400">Nuestra cultura nos mueve todos los días a brindarnos por los demás. Confiamos que nuestro éxito sólo se alcanza a través del éxito, primero de nuestras Marcas, Corredores y Agentes. Ellos lo logran si tienen éxito sostenible con sus clientes. Buscamos el éxito sostenido en relaciones a lo largo del tiempo..</p>
           </a>
           <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 bg-gray-800  hover:bg-gray-700">
-          <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path></svg>
+          <svg class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path></svg>
             <h5 class="mb-2 text-2xl font-bold tracking-tight  text-white"> En nuestros métodos y sistemas</h5>
             <p class="font-normal  text-gray-400">Somos la compañía número 1 en entrenamiento y formación en el mundo. Ofrecemos métodos comprobados para que agentes y corredores matriculados puedan crecer en productividad y eficiencia, una prioridad de nuestros clientes.</p>
           </a>
           <a href="#" class="block p-5 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 bg-gray-800  hover:bg-gray-700">
-          <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+          <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
             <h5 class="mb-2 text-2xl font-bold tracking-tight  text-white">En el resultado</h5>
             <p class="font-normal  text-gray-400">Nos ocupamos de desarrollar carreras inmobiliarias donde tu marca, tu identidad y tu desarrollo en el tiempo, sean lo más importante. <dd></dd>Para ello promovemos el estudio de la carrera de corredor inmobiliario, para que tu negocio alcance el máximo nivel posible.</p>
           </a>
