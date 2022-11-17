d55c98c16ad536eb9ae767f842018e7f0c279cc3
enzo2022 <enzo142014@hotmail.com>
2022-11-17 10:30:31 -0300

    logica de SihnUp

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -87,3 +87,29 @@ export function getCitiesA() {
     }
 }
 
+export function postSignUp(formData) {
+    return async function(dispatch) {
+        let { email, userName,  password, photo,
+            cellphone } = formData;
+        const fixedData = {
+            email,
+             userName,
+               password,
+               photo,
+              cellphone 
+      
+        }
+        console.log(fixedData)
+        await axios.post("http://localhost:3001/users/signUp", fixedData)
+        dispatch({type: "POST_SIGNUP", payload: fixedData})
+    }
+}
+
+
+
+// export function postSignUp(payload){
+//     return async function(dispatch){
+//        const postVideogame = await axios.post('http://localhost:3001/users/signUp',payload);
+//        return postVideogame;
+//     };
+// }
