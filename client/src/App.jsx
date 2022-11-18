132580ebeeea489429326105200c7169a6636e3a
jhoniernem <jhonny30ca@gmail.com>
2022-11-18 07:50:37 -0500

    02

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route,Routes } from "react-router-dom";
 import {useEffect} from 'react';
-import {useDispatch,useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {getallProperties, getCities,getCitiesA,addFavorites} from './redux/actions/index';
 import Landing from "./pages/landing/Landing.jsx";
 import Home from './pages/home/Home.jsx';
@@ -11,16 +11,13 @@ import LogIn from "./pages/logIn/LogIn";
 import SignUp from "./pages/signup/SignUp";
 
 function App() {
-  const {properties} = useSelector(state => state);
   const dispatch = useDispatch()
     useEffect(() => {
       dispatch(getallProperties())
       dispatch(getCities())
       dispatch(getCitiesA())
       const StorageFavorite = localStorage.getItem('favorite')
-        if(StorageFavorite.split('&')){
-
-        }else if (StorageFavorite?.length) {
+      if (StorageFavorite?.length) {
           dispatch(addFavorites(StorageFavorite.split('&')))
           localStorage.setItem('favorite',``)
         }
