4b02c6b4b1eedaba752688a8c689dbda50a46aae
enzo2022 <enzo142014@hotmail.com>
2022-11-19 12:43:40 -0300

    git push --set-upstream origin aurh0

--- client/src/pages/profaid.jsx
+++ client/src/pages/profaid.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useDispatch } from "react-redux";
-import SignUp from "./signup/SignUp";
-
-
-
 
 export default function Profile  () {
-const dispatch = useDispatch()
-
-
 
   const { user, isAuthenticated, isLoading } = useAuth0();
-  dispatch(SignUp(user)) 
+     
   if (isLoading) {
     return <div>Loading...</div>;
   }
