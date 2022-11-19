21e8a96462886ab20007bed7ff82c71c0b3f2e54
musicologo07 <106623570+musicologo07@users.noreply.github.com>
2022-11-19 19:07:47 -0300

    dale que va

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {store} from './redux/store/index'
+import { store } from "./redux/store/index";
+import axios from "axios";
+axios.defaults.baseURL = process.env.CLIENT_PF || "http://localhost:3001/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="78341286584-ginpoaiq2llgaup3gkr9pg6spnoe7lfq.apps.googleusercontent.com">
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}> 
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>
   </GoogleOAuthProvider>
 );
