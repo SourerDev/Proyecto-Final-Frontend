afb062642c83515ff9d19251cf68f6f290b3fbf7
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2022-11-26 01:30:59 -0300

    connection with back (in process)

--- client/src/pages/editUser/EditUser.jsx
+++ client/src/pages/editUser/EditUser.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { loadUserInfo } from "../../redux/actions/index";
+import { isValidUser } from "../../utils/isValidUser";
 
 export default function EditUser() {
   const { user } = useSelector((state) => state);
@@ -9,30 +10,34 @@ export default function EditUser() {
     email: user.email,
     photo: user.photo,
   });
+  const [errs, setErrs] = useState({})
   const dispatch = useDispatch();
 
   useEffect(() => {
     console.log(newUser);
-  }, [newUser]);
+    console.log(errs)
+  }, [newUser, errs]);
 
   function onUserChange(e) {
-    if (e.target.value.length) {
       setNewUser({
         ...newUser,
         [e.target.name]: e.target.value,
       });
-    }
+      setErrs(isValidUser({
+        ...newUser,
+        [e.target.name]: e.target.value,
+      }))
   }
 
   return (
     <div className="flex h-[87] flex-col items-center">
       <p className="self-right">Editar usuario</p>
       <div className="w-16 h-16 rounded-full overflow-hidden">
-        <img className="w-full rounded-full" src={user?.photo || "https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png"} alt="" />
+        <img className="w-full rounded-full" src={newUser.photo || "https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png"} alt="" />
       </div>
       <div>
         <label
-          for="first_name"
+          for="userName"
           className="block m-1 text-sm font-medium text-gray-900 dark:text-white"
         >
           Nombre de Usuario
@@ -41,15 +46,15 @@ export default function EditUser() {
           type="text"
           name="userName"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder={user?.userName || "Sin User Name"}
-          value={user.userName}
-          onChange={onUserChange}
+          placeholder={user?.userName || "sin nombre de usuario"}
+          value={newUser.userName}
+          onChange={(e) => onUserChange(e)}
         />
-
+        {errs.userName && <p className=" text-left px-6 mt-2 text-sm text-red-600 dark:text-red-500">{errs.userName}</p>}
       </div>
       <div>
       <label
-          for="first_name"
+          for="email"
           className="block m-1 text-sm font-medium text-gray-900 dark:text-white"
         >
           Email
@@ -59,13 +64,14 @@ export default function EditUser() {
           name="email"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder={user?.email}
-          value={user.email}
-          onChange={onUserChange}
+          value={newUser.email}
+          onChange={(e) => onUserChange(e)}
         />
+        {errs.email && <p className=" text-left px-6 mt-2 text-sm text-red-600 dark:text-red-500">{errs.email}</p>}
       </div>
       <div>
       <label
-          for="first_name"
+          for="photo"
           className="block m-1 text-sm font-medium text-gray-900 dark:text-white"
         >
           Foto
@@ -74,13 +80,23 @@ export default function EditUser() {
           type="text"
           name="photo"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder={user?.photo}
-          value={user.photo}
-          onChange={onUserChange}
+          placeholder={"se le asignara un avatar por defecto"}
+          value={newUser.photo}
+          onChange={(e) => onUserChange(e)}
         />
 
       </div>
-        <button className="m-2 p-2 border rounded-sm hover:rounded-lg" onClick={() => dispatch(loadUserInfo)}>actualizar</button>
+        <button  
+          disabled={Object.values(errs).length ? true : false} 
+          className="m-2 p-2 border rounded-sm hover:rounded-lg" 
+          onClick={() => {
+            dispatch(loadUserInfo({
+              ...user,
+              ...newUser,
+              photo: newUser.photo.length ? newUser.photo : "https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png"
+            }))
+            
+          }}>actualizar</button>
     </div>
   );
 }
---
+++ client/src/utils/isValidUser.js
@@ -0,0 +1,11 @@
+
+export function isValidUser(user) {
+  let errs = {}
+
+  if(!user.email.length) errs.email = "debe ingresar un nuevo email o dejar el actual"
+  else if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(user.email)) errs.email = "ingrese un email valido";
+
+  if(!user.userName.length) errs.userName = 'ingrese un nuevo nombre de usuario';
+
+  return errs
+}
