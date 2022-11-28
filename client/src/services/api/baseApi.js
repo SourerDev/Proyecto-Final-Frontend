bffdfb498cb4349ec2f9294adf39076094fe447c
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2022-11-28 17:28:35 -0300

    ahora si

--- client/src/services/api/baseApi.js
+++ client/src/services/api/baseApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = "http://localhost:3001"
+export const API_URL = "https://proyecto-final-frontend-jhoniernem.vercel.app/"
 /* Api: http://localhost:3001*/
 
 export const apiProperties = axios.create({
