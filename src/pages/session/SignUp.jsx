8429f8a9ca0fc1d67a5b404908f6ea97570b7336
jhoniernem <jhonny30ca@gmail.com>
2023-05-26 11:06:14 -0500

    refactor : Error messge

--- src/pages/session/SignUp.jsx
+++ src/pages/session/SignUp.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ApiPropYou, addAuthorizationWithToken } from "../../services";
 import { actionsUser } from "../../redux2.0/reducers";
@@ -35,9 +35,42 @@ export function SignUp() {
     );
   }
 
+  async function onSubmitSignUp(evt) {
+    evt.preventDefault();
+    try {
+      const response = await ApiPropYou.signUp(data);
+
+      const { user, token } = response.data;
+      dispatch(actionsUser.setUser(user));
+      saveInStorage("token", token);
+
+      swal({
+        title: "Ecxelente",
+        text: "Usuario Creado!",
+        icon: "success",
+      });
+      addAuthorizationWithToken(token);
+      navigate(`/user/${user.idUser}`);
+    } catch ({ response, ...error }) {
+      let text = "...";
+      if (response?.data?.Error) text = response?.data?.Error;
+      else {
+        if (response?.data === "User already exist")
+          setErrs({ email: "Correo registrado previamente" });
+        text = response?.data;
+      }
+
+      swal({
+        title: "Error",
+        text: `Ha sucedido un error \n ${text}`,
+        icon: "error",
+      });
+    }
+  }
+
   return (
     <div className="h-[87vh]">
-      <section className="h-full">
+      <section className="h-full my-4">
         <div className="px-6 h-full text-gray-800">
           <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
             <div className=" hidden lg:flex grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
@@ -48,29 +81,7 @@ export function SignUp() {
               />
             </div>
             <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-              <form
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  try {
-                    const response = await ApiPropYou.signUp(data);
-
-                    console.log(response);
-                    const { user, token } = response.data;
-                    dispatch(actionsUser.setUser(user));
-                    saveInStorage("token", token);
-
-                    swal({
-                      title: "Ecxelente",
-                      text: "Usuario Creado!",
-                      icon: "success",
-                    });
-                    addAuthorizationWithToken(token);
-                    navigate(`/user/${user.idUser}`);
-                  } catch (e) {
-                    console.log(e);
-                  }
-                }}
-              >
+              <form onSubmit={onSubmitSignUp}>
                 <div className="mb-6">
                   <Input
                     name="email"
@@ -79,15 +90,7 @@ export function SignUp() {
                     placeholder="correo electronico"
                     onChange={(e) => handleChange(e)}
                   />
-                  {/* <input
-                    name="email"
-                    type="email"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    id="FormControlInput1"
-                    placeholder="correo electronico"
-                    onChange={(e) => handleChange(e)}
-                  /> */}
-                  {errs.email && <p className="text-red-600">{errs.email}</p>}
+                  <ErrorMessage error={errs.email} />
                 </div>
 
                 <div className="mb-6">
@@ -98,15 +101,7 @@ export function SignUp() {
                     placeholder="first name"
                     onChange={(e) => handleChange(e)}
                   />
-                  {/* <input
-                    name="fName"
-                    type="fName"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    id="FormControlInput3"
-                    placeholder="first name"
-                    onChange={(e) => handleChange(e)}
-                  /> */}
-                  {errs.fName && <p className="text-red-600">{errs.fName}</p>}
+                  <ErrorMessage error={errs.fName} />
                 </div>
 
                 <div className="mb-6">
@@ -117,7 +112,7 @@ export function SignUp() {
                     placeholder="last name"
                     onChange={(e) => handleChange(e)}
                   />
-                  {errs.lName && <p className="text-red-600">{errs.lName}</p>}
+                  <ErrorMessage error={errs.lName} />
                 </div>
 
                 <div className="mb-6">
@@ -126,9 +121,7 @@ export function SignUp() {
                     placeholder="password"
                     onChange={(e) => handleChange(e)}
                   />
-                  {errs.password && (
-                    <p className="text-red-600">{errs.password}</p>
-                  )}
+                  <ErrorMessage error={errs.password} />
                 </div>
                 <div className="mb-6">
                   <PasswordInput
@@ -146,29 +139,22 @@ export function SignUp() {
                     placeholder="cellphone"
                     onChange={(e) => handleChange(e)}
                   />
-                  {errs.cellphone && (
-                    <p className="text-red-600">{errs.cellphone}</p>
-                  )}
+                  <ErrorMessage error={errs.cellphone} />
                 </div>
 
-                <div className="flex flex-col items-start text-center lg:text-left lg:h-20 grid lg:grid-cols-3 lg:gap-4 lg:content-center">
+                <div className="flex-col items-start text-center gap-2 lg:text-left lg:h-20 grid lg:grid-cols-3 lg:gap-4 lg:content-center">
                   <button
-                    disabled={
-                      !Object.keys(errs).length && data.email.length
-                        ? false
-                        : true
-                    }
+                    disabled={!(!Object.keys(errs).length && data.email.length)}
                     type="submit"
-                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-red-400 disabled:cursor-not-allowed"
+                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-red-400 disabled:cursor-not-allowed disabled:"
                   >
                     Crear Usuario
                   </button>
                   <div>
-                    {" "}
                     <Link to="/sign-in">
-                      <button className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-red-400 disabled:cursor-not-allowed">
+                      <span className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-red-400 disabled:cursor-not-allowed">
                         Iniciar sesión
-                      </button>
+                      </span>
                     </Link>
                   </div>
                 </div>
@@ -178,5 +164,18 @@ export function SignUp() {
         </div>
       </section>
     </div>
+  );
+}
+
+function ErrorMessage({ error, className, ...props }) {
+  if (!error?.length) return null;
+
+  return (
+    <p
+      className={`text-sm text-red-600 m-y-1 font-semibold px-4 ${className}`}
+      {...props}
+    >
+      {error[0].toUpperCase() + error.slice(1)}
+    </p>
   );
 }
