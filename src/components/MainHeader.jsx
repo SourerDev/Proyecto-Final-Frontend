6d6c50a955ba1d55995ccc96924c38107b566e5a
jhoniernem <jhonny30ca@gmail.com>
2023-06-05 09:00:58 -0500

    Added classname to main header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 
-export function MainHeader({ children }) {
+export function MainHeader({ className, children }) {
   return (
-    <div className="h-screen min-h-[500px] bg-[url('https://img.freepik.com/foto-gratis/familia-moviendose-usando-cajas_1157-35480.jpg?w=2000')] bg-cover bg-no-repeat">
+    <div
+      className={`h-screen min-h-[500px] bg-[url('https://img.freepik.com/foto-gratis/familia-moviendose-usando-cajas_1157-35480.jpg?w=2000')] bg-cover bg-no-repeat ${className}`}
+    >
       {children}
     </div>
   )
@@ -10,5 +12,6 @@ export function MainHeader({ children }) {
 
 //
 MainHeader.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.any,
 }
