13775d301d3e2da1bc89520e303f5e7c5a90227e
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-06-19 13:49:55 -0300

    add services carousel

--- src/components/carousels/Carousel.jsx
+++ src/components/carousels/Carousel.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { findNameCity } from '../../utils/autocompleteUtils'
 import { Link, useNavigate } from 'react-router-dom'
 import { filterProperties } from '../../redux/actions'
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 
 /*
   ABCD -> BCDA ->
@@ -14,313 +14,87 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 */
 
 export function Carousel({ children }) {
-  const [renderChildren, setRenderChildren] = useState(children)
-  const [isLoading, setIsLoading] = useState(false)
-  const [sizeCarousel, setSizeCarousel] = useState(0)
+  const carouselRef = useRef(null)
 
-  function next() {
-    if (renderChildren.length > 0) {
-      const children = [...renderChildren]
+  /* function next() {
+    if (carouselRef?.current?.children.length > 0) {
+      const firstElement = carouselRef.current.children[carouselRef.current.children.length - 1]
       const eliminado = children.shift()
       children.push(eliminado)
-      setRenderChildren(children)
     }
-  }
-
-  function previus() {
-    if (renderChildren.length > 0) {
-      const children = [...renderChildren]
-      const eliminado = children.pop()
-      children.unshift(eliminado)
-      setRenderChildren(children)
-    }
-  }
-
-  return (
-    <div
-      className={
-        'relative mb-6 flex min-h-[150px] w-full flex-row flex-nowrap overflow-hidden border-4 border-y-2 border-red-500'
-      }
-    >
-      <button
-        disabled={isLoading}
-        onClick={previus}
-        className="absolute top-1/2 left-2 grid aspect-square -translate-y-1/2 transform place-content-center rounded-full border border-red-500"
-      >
-        <ArrowLeftIcon className="h-6 w-8" />
-      </button>
-      <button
-        disabled={isLoading}
-        onClick={next}
-        className="absolute top-1/2 right-2 grid aspect-square -translate-y-1/2 transform place-content-center rounded-full border border-red-500"
-      >
-        <ArrowRightIcon className="h-6 w-8" />
-      </button>
-      <span className="absolute left-1/2 top-[105%] h-4  w-8 -translate-x-1/2 transform border border-red-500"></span>
-      {renderChildren}
-    </div>
-  )
-}
-
-export function Item({ children, className }) {
-  return (
-    <div
-      className={`flex min-w-full items-center justify-center border-x-2 sm:min-w-[50%]  md:min-w-[33.333%] md:max-w-[25%] lg:min-w-[25%] lg:max-w-[33.333%]${className}`}
-    >
-      {children}
-    </div>
-  )
-}
-
-export default function CarouselOld({ title, images }) {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const { citiesA } = useSelector((state) => state)
-
-  const slideshow = useRef(null)
-  const interval = useRef(null)
-
+  } */
   const next = () => {
-    if (slideshow?.current?.children?.length > 0) {
-      const firstElement = slideshow?.current?.children[0]
-      slideshow.current.style.transition = '300ms ease-out all'
+    if (carouselRef?.current?.children?.length > 0) {
+      const firstElement = carouselRef?.current?.children[0]
+      carouselRef.current.style.transition = '300ms ease-out all'
 
-      const size = slideshow.current.children[0].offsetWidth
+      const size = carouselRef.current.children[0].offsetWidth
 
-      slideshow.current.style.transform = `translatex(-${size}px)`
-      /* [][]*/
+      carouselRef.current.style.transform = `translatex(-${size}px)`
+
       const transicion = () => {
-        slideshow.current.style.transition = 'none'
-        slideshow.current.style.transform = 'translatex(0px)'
+        carouselRef.current.style.transition = 'none'
+        carouselRef.current.style.transform = 'translatex(0px)'
 
-        slideshow.current.appendChild(firstElement)
-        slideshow.current.removeEventListener('transitionend', transicion)
+        carouselRef.current.appendChild(firstElement)
+        carouselRef.current.removeEventListener('transitionend', transicion)
       }
 
-      slideshow.current.addEventListener('transitionend', transicion)
+      carouselRef.current.addEventListener('transitionend', transicion)
     }
   }
-
-  const previus = () => {
-    if (slideshow?.current?.children.length > 0) {
+  function previus() {
+    if (carouselRef?.current?.children.length > 0) {
       const endElement =
-        slideshow.current.children[slideshow.current.children.length - 1]
-      slideshow.current.insertBefore(endElement, slideshow?.current?.firstChild)
+        carouselRef.current.children[carouselRef.current.children.length - 1]
+      carouselRef.current.insertBefore(
+        endElement,
+        carouselRef?.current?.firstChild
+      )
 
-      slideshow.current.style.transition = 'none'
+      carouselRef.current.style.transition = 'none'
 
-      const size = slideshow.current.children[0].offsetWidth
-      slideshow.current.style.transform = `translate(-${size}px)`
+      const size = carouselRef.current.children[0].offsetWidth
+      carouselRef.current.style.transform = `translate(-${size}px)`
 
       setTimeout(() => {
-        slideshow.current.style.transition = '300ms ease-out all'
-        slideshow.current.style.transform = 'translatex(0)'
+        carouselRef.current.style.transition = '300ms ease-out all'
+        carouselRef.current.style.transform = 'translatex(0)'
       }, 30)
     }
   }
 
   return (
-    <div className="flex w-full flex-col items-center justify-center overflow-hidden py-2">
-      <div className="relative flex w-full justify-between px-1 sm:w-11/12 sm:px-3">
-        <Title>{title || 'Title'}</Title>
-        <button
-          className="hidden hover:p-1 hover:text-blue-500 sm:block"
-          onClick={() => {
-            dispatch(filterProperties(images))
-            navigate('/home')
-          }}
-        >
-          Ver Mas »
-        </button>
+    <div
+      className= 'relative w-full overflow-hidden p-0 m-0 '
+    >
+      <button
+        onClick={previus}
+        className=" group absolute top-1/2  left-0 z-50 grid aspect-square -translate-y-1/2 transform place-content-center  rounded-full "
+      >
+        <ChevronLeftIcon className="h-6 w-8 stroke-gray-300/50 group-hover:stroke-gray-500/50 transition-all" />
+      </button>
+      <button
+        onClick={next}
+        className=" group absolute top-1/2 right-0 z-50 grid aspect-square -translate-y-1/2 transform place-content-center rounded-full"
+      >
+        <ChevronRightIcon className="h-6 w-8 stroke-gray-300/50 group-hover:stroke-gray-500/50 transition-all" />
+      </button>
+      <div ref={carouselRef} className="flex h-[100%] w-full z-40">
+        {children}
       </div>
-      {images?.length > 0 && (
-        <Main className="shadow">
-          <Slideshow
-            ref={slideshow}
-            onMouseEnter={(e) => {
-              clearInterval(interval.current)
-              interval.current = null
-            }}
-            onMouseLeave={(e) => {
-              interval.current = setInterval(() => {
-                next()
-              }, 5000)
-            }}
-          >
-            {images?.length > 0 &&
-              images.map((element, i) => (
-                <Slide key={i}>
-                  <Link to={`/detail/${element.id}`}>
-                    <img
-                      src={element.images[0]}
-                      alt={element.id}
-                      className=""
-                    />
-                  </Link>
-                  {element?.id && (
-                    <TextSlide>
-                      <svg
-                        className=""
-                        fill="#fff"
-                        width="24"
-                        height="24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                      >
-                        <path d="M12 10c-1.104 0-2-.896-2-2s.896-2 2-2 2 .896 2 2-.896 2-2 2m0-5c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3m-7 2.602c0-3.517 3.271-6.602 7-6.602s7 3.085 7 6.602c0 3.455-2.563 7.543-7 14.527-4.489-7.073-7-11.072-7-14.527m7-7.602c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602" />
-                      </svg>
-                      <p>{findNameCity(citiesA, element.idCity)}</p>
-                    </TextSlide>
-                  )}
-                </Slide>
-              ))}
-          </Slideshow>
-          <Controls>
-            <Button r className="left-0" onClick={previus}>
-              <svg
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              >
-                <path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" />
-              </svg>
-            </Button>
-            <Button
-              className="right-0 flex items-center justify-end"
-              onClick={next}
-            >
-              <svg
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              >
-                <path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
-              </svg>
-            </Button>
-          </Controls>
-        </Main>
-      )}
     </div>
   )
 }
-
-const Main = styled.div`
-  overflow: hidden;
-  position: relative;
-  width: 90%;
-  border-radius: 0.2rem;
-  @media screen and (max-width: 640px) {
-    width: 100%;
-  }
-`
-
-const Slideshow = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-`
-
-const Slide = styled.div`
-  min-width: 100%;
-  overflow: hidden;
-  transition: 0.3s ease all;
-  z-index: 10;
-  max-height: 250px;
-  position: relative;
-
-  img {
-    padding: 0 1px;
-    width: 100%;
-    height: 100%;
-    vertical-align: top;
-    transition: all 1s ease-in-out;
-    &:hover {
-      transform: scale(1.1);
-      filter: grayscale(80%);
-    }
-  }
-
-  @media screen and (min-width: 200px) {
-    min-width: 100%;
-  }
-
-  @media screen and (min-width: 400px) {
-    min-width: 50%;
-  }
-  @media screen and (min-width: 800px) {
-    min-width: 33.33333333%;
-  }
-  @media screen and (min-width: 1200px) {
-    min-width: 25%;
-  }
-`
-
-const TextSlide = styled.div`
-  background: rgba(0, 0, 0, 0.5);
-  color: #fff;
-  width: 100%;
-  padding: 10px 5px;
-  text-align: center;
-  position: absolute;
-  bottom: 0px;
-  font-weight: 900;
-  display: flex;
-  justify-content: right;
-  align-items: center;
-  @media screen and (max-width: 700px) {
-    font-size: small;
-    svg {
-      display: none;
-    }
-  }
-`
-
-const Controls = styled.div`
-  position: absolute;
-  top: 0%;
-  z-index: 20;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-`
-
-const Button = styled.button`
-  pointer-events: all;
-  background: none;
-  border: none;
-  cursor: pointer;
-  outline: none;
-  width: 50px;
-  height: 100%;
-  text-align: center;
-  position: absolute;
-  transition: 0.3 ease all;
-  &:hover {
-    path {
-      fill: #b7b4b4;
-    }
-  }
-`
-
-const Title = styled.h1`
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  font-size: 1.7rem;
-  font-weight: 600;
-  color: #3c52c3;
-  text-shadow: 0px 0px 9px #5f95d6;
-  @media screen and (max-width: 700px) {
-    position: absolute;
-    font-size: larger;
-    font-weight: 600;
-    left: 5px;
-    bottom: -25px;
-    z-index: 10;
-  }
-`
+export function Item({ children, className }) {
+  return (
+    <div
+      className={`flex h-full min-w-full items-center justify-center sm:min-w-[50%]  md:min-w-[33.333%] md:max-w-[25%] lg:min-w-[25%] lg:max-w-[33.333%] ${className}`}
+    >
+      {children}
+    </div>
+  )
+}
 
 Carousel.propTypes = {
   children: PropTypes.any,
--- src/components/carousels/ServicesPropertyCarousel.jsx
+++ src/components/carousels/ServicesPropertyCarousel.jsx
@@ -21,19 +21,24 @@ const services = [
     firm: 'InterNaxa S.A - Argentina',
     cellphone: '44444',
   },
+  {
+    service: 'Proveedor de servicios de telecomunicacion',
+    firm: 'InterNaxa S.A - Argentina',
+    cellphone: '5555',
+  },
 ]
 
 export function ServicesPropertyCarousel() {
   return (
-    <div>
-      <h2 className="mt-2 rounded-lg bg-gray-800 px-4 text-left text-white">
+    <div className='p-3 '>
+      <h2 className="mt-2 rounded-lg bg-gray-800 px-4 text-left text-white mb-1">
         Servicios
       </h2>
       <Carousel>
         {services.map((service, i) => (
           <Item
             key={i}
-            className="flex flex-col rounded-lg border border-yellow-500 bg-white p-3 text-center text-gray-800"
+            className="p-3 flex flex-col bg-white  text-center text-gray-800  justify-around border-x border-gray-800"
           >
             <p>{service.firm}</p>
             <p>{service.service}</p>
@@ -41,6 +46,7 @@ export function ServicesPropertyCarousel() {
           </Item>
         ))}
       </Carousel>
+
     </div>
   )
 }
