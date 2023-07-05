d1cd1cde32fdbbb01ec8acf644c0f0b24ef1c3f8
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-07-05 19:50:56 -0300

    finish SvedProperties.jsx styles

--- src/pages/savedProperties/SavedProperties.jsx
+++ src/pages/savedProperties/SavedProperties.jsx
@@ -4,10 +4,12 @@ import { LoaderIcon } from '../../components/loaders/Loader'
 import { ApiPropYou } from '../../services'
 import { actionsUser } from '../../redux2.0/reducers'
 import { PropertyCard } from '../../components/cards/PropertyCard'
+import { Link } from 'react-router-dom'
+import { Button } from '../../components/form/buttons/Button'
+import { GoBackButton } from '../../components/form/buttons/GoBack'
 /* import Card from '../card/Card.jsx'
 import callsApi from '../../services/index.js'
 import { findNameCity } from '../../utils/autocompleteUtils.js'
-import { Link } from 'react-router-dom'
 import Nav from '../../components/nav-bar/Nav' */
 
 export function SavedProperties() {
@@ -39,14 +41,16 @@ export function SavedProperties() {
     }
   }
   return (
-    <div className="px-4">
-      {/* {!publications && (
+    <>
+      <GoBackButton className="m-2 rounded-md" />
+      {!publications && (
         <LoaderIcon className="fixed bottom-2 left-2 w-[40px]" />
-      )} */}
-      { publications?.length ? (
-        <div className="gap-x-6 gap-y-4 sm:flex sm:flex-wrap sm:justify-center pt-3">
+      )}
+      {publications?.length ? (
+        <div className="gap-x-6 gap-y-4 py-3 sm:flex sm:flex-wrap sm:justify-center">
           {publications.map((publication, i) => {
-            const { idPublication, modality, price, id } = publication.Publication
+            const { idPublication, modality, price, id } =
+              publication.Publication
             const mainData = {
               idPublication,
               modality,
@@ -80,56 +84,19 @@ export function SavedProperties() {
             )
           })}
         </div>
-      ) : (
-        <div>no hay length</div>
+      ) : null}
+      {publications && !publications.length && (
+        <div className="flex justify-center items-center border-2  py-8">
+          <div className="w-fit flex flex-col items-center gap-3 ">
+            <h1>Lo sentimos, no tienes publicaciones guardadas</h1>
+            <Link to="/home" className=''>
+              <Button>
+                <p>ver propiedades</p>
+              </Button>
+            </Link>
+          </div>
+        </div>
       )}
-      {/* <Nav />
-      <div className=" my-5">
-        <Link to="/home">
-          <button className="whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
-            Volver
-          </button>
-        </Link>
-      </div>
-      <div className="h-14  rounded-lg bg-gray-800 text-center  text-3xl font-bold text-white ">
-        <p className="inline-block align-middle">Mis favoritos</p>
-      </div>
-
-      <div className="flex flex-wrap justify-center sm:flex-row lg:shadow-2xl ">
-        {favorites?.length > 0 ? (
-          favorites.map((ele, i) => (
-            <div key={i} className="my-2 px-4 lg:px-9">
-              <Card
-                key={ele.id}
-                id={ele.id}
-                favorite={true}
-                city={findNameCity(citiesA, ele.idCity)}
-                modality={ele.modality}
-                address={ele.address}
-                price={ele.price}
-                images={ele.images}
-                garage={ele.garage}
-                idUser={id_User}
-                userProperty={ele.User}
-              />
-            </div>
-          ))
-        ) : (
-          <h1 className="m-2 flex flex-col text-center text-2xl ">
-            Sin Favoritos
-            <dd>
-              Para agregar favoritos a tu lista, seleccionelos en el inicio.
-              <Link to="/home">
-                <div className="">
-                  <button className="whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
-                    Inicio
-                  </button>
-                </div>
-              </Link>
-            </dd>
-          </h1>
-        )}
-      </div> */}
-    </div>
+    </>
   )
 }
