2f3d2fe31727f7ad7f8bea4ef7220730c06f7523
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-07-11 15:14:13 -0300

    finish detail basics styles

--- src/components/cards/OwnerCard.jsx
+++ src/components/cards/OwnerCard.jsx
@@ -10,28 +10,32 @@ import {
 } from '@heroicons/react/24/outline'
 import { Alerts } from '../../utils'
 
-export function OwnerCard({ User }) {
+export function OwnerCard({ User, className }) {
   return (
     <Card className="w-full">
-      <div className="flex items-center gap-x-2 ">
+      <div className="flex items-center gap-x-4 lg:gap-x-2 xl:gap-x-6">
         <Avatar
+          className=" "
           avatar={User.photo}
           name={`${User.fName} ${User.lName}`}
           active={User.active}
         />
-        <p className="flex flex-col ">
+        <p className="flex flex-col md:flex-row md:gap-x-4 lg:gap-x-0
+        lg:items-center  ">
           <span className="text-lg font-bold">{`${User.fName} ${User.lName}`}</span>
           <span className="text-gray-600">@{User.userName}</span>
         </p>
-        <p className=" flex items-center gap-x-1">
+        <p className=" ml-6 flex items-center   md:ml-auto md:mr-10 lg:mr-0 lg:w-[42%] lg:p-0">
           <DevicePhoneMobileIcon
             title={User.cellphone}
             className="h-auto w-6 stroke-gray-800"
           />{' '}
-          <span>{User.cellphone || '+ 57 3123122626'}</span>
+          <span className=''>{User.cellphone || '+57 3123122626'}</span>
         </p>
-        {//considerar si implementaremos un sitema real de valoracion con estrellas
-        /* <p className="flex-grow border">** No stars **</p> */}
+        {
+          //considerar si implementaremos un sitema real de valoracion con estrellas
+          /* <p className="flex-grow border">** No stars **</p> */
+        }
       </div>
       <div className="my-2 flex justify-between">
         <p className=" flex items-center gap-x-2">
@@ -52,12 +56,15 @@ function CardMessage() {
   return (
     <div className="flex flex-col gap-y-2">
       <TextArea
-        className='border border-gray-800 p-2 rounded-sm'
+        className="rounded-sm border border-gray-800 p-2"
         placeholder="Mensaje"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      <Button className='w-fit my-0 mx-auto' onClick={() => Alerts.soon({ text: 'Pronto podras contactar' })}>
+      <Button
+        className="my-0 mx-auto w-fit"
+        onClick={() => Alerts.soon({ text: 'Pronto podras contactar' })}
+      >
         Contactar
       </Button>
     </div>
--- src/pages/property/PropertyDetails.jsx
+++ src/pages/property/PropertyDetails.jsx
@@ -38,83 +38,85 @@ export function PropertyDetails() {
   }, [dispatch])
 
   if (!publication?.idPublication) return <div>...Loading</div>
-
   return (
-    <div className="flex flex-col p-4 xl:min-h-screen    xl:px-4 xl:py-8 xl:border-2 xl:border-violet-500">
-      <div className="xl:w-[56%] xl:inline-block ">
-        <CarrouselDetail className="" images={[...Property.photos]} />
-      </div>
-
-      <div className="my-4 flex flex-col items-start gap-[1rem]  xl:w-[42%] xl:border-4 xl:border-red-500">
-        <div className="w-full rounded border p-4 shadow-md ">
-          <p className="pl-1 text-lg font-semibold text-indigo-600 xl:ml-auto xl:mr-2 xl:w-fit">
-            {type[Property.type]} en {modality[publication.modality]}
-          </p>
-          <div className="mt-2 mb-4 border-l-4 border-indigo-600 p-1 xl:flex">
-            <p className="flex pl-1 text-2xl">
-              {publication.price} <p className="ml-[0.3rem] font-bold">USD</p>
+    <div className="min-h-screen border p-4">
+      <div className="mb-4 space-y-3 xl:flex xl:gap-x-2 xl:space-y-0">
+        <div className="xl:flex-1">
+          <CarrouselDetail className="" images={[...Property.photos]} />
+        </div>
+        <div className="xl:flex flex-col gap-y-4 ">
+          <div className='rounded border p-4 shadow-md xl:h-fit xl:flex-1'>
+            <p className="pl-1 text-lg font-semibold text-indigo-600 xl:ml-auto xl:mr-2 xl:w-fit">
+              {type[Property.type]} en {modality[publication.modality]}
             </p>
-          </div>
-
-          <div className="flex border-y-2 border-gray-400">
-            <div className="flex py-2 md:gap-4 xl:w-full xl:items-center">
-              <p className="w-fit border-r-2 border-gray-500 pr-0.5 text-xl font-medium md:pr-6">{`${Property.City.string}`}</p>
-              <p className="ml-1  pl-2 italic text-gray-600">
-                {Property.address}
+            <div className="mt-2 mb-4 border-l-4 border-indigo-600 p-1 xl:flex">
+              <p className="flex pl-1 text-2xl">
+                {publication.price}{' '}
+                <p className="ml-[0.3rem] font-bold">USD</p>
+              </p>
+            </div>
+            <div className="flex border-y-2 border-gray-400">
+              <div className="flex py-2 md:gap-4 xl:w-full xl:items-center">
+                <p className="w-fit border-r-2 border-gray-500 pr-1.5 text-xl font-medium md:pr-6">{`${Property.City.string}`}</p>
+                <p className="ml-1  pl-2 italic text-gray-600">
+                  {Property.address}
+                </p>
+              </div>
+            </div>
+            <div className="p-3 text-xl xl:flex xl:justify-around">
+              <p className="flex items-center gap-2 text-center">
+                <CurrencyDollarIcon className="h-6 w-6" />
+                <p className="flex text-lg">
+                  Baños{' '}
+                  <p className="ml-2 text-lg text-gray-500">
+                    {Property.bathrooms}
+                  </p>
+                </p>
+              </p>
+              <p className="flex items-center gap-2 text-center">
+                <RectangleGroupIcon className="h-6 w-6" />
+                <p className="flex text-lg">
+                  Cuartos{' '}
+                  <p className="ml-2 text-lg text-gray-500">
+                    {Property.bedrooms}
+                  </p>
+                </p>
+              </p>
+              <p className="flex items-center gap-2 text-center">
+                <RectangleGroupIcon className="h-6 w-6" />
+                <p className="flex text-lg">
+                  Metros²{' '}
+                  <p className="ml-2 text-lg text-gray-500">
+                    {Property.squareMeters}
+                  </p>
+                </p>
               </p>
             </div>
           </div>
-          <div className="p-3 text-xl xl:flex xl:justify-around">
-            <p className="flex items-center gap-2 text-center">
-              <CurrencyDollarIcon className="h-6 w-6" />
-              <p className="flex text-lg">
-                Baños{' '}
-                <p className="ml-2 text-lg text-gray-500">
-                  {Property.bathrooms}
-                </p>
-              </p>
-            </p>
-            <p className="flex items-center gap-2 text-center">
-              <RectangleGroupIcon className="h-6 w-6" />
-              <p className="flex text-lg">
-                Cuartos{' '}
-                <p className="ml-2 text-lg text-gray-500">
-                  {Property.bedrooms}
-                </p>
-              </p>
-            </p>
-            <p className="flex items-center gap-2 text-center">
-              <RectangleGroupIcon className="h-6 w-6" />
-              <p className="flex text-lg">
-                Metros²{' '}
-                <p className="ml-2 text-lg text-gray-500">
-                  {Property.squareMeters}
-                </p>
-              </p>
-            </p>
-          </div>
-        </div>
-
-        <div className="lg:flex lg:gap-4 xl:border-2 xl:border-violet-500">
-          <div className="overflow-scroll  rounded border border-2 border-violet-700 px-3 py-1 text-center shadow-md md:mt-2 md:h-[20rem] md:h-fit xl:h-[34.8%] lg:flex-1 lg:mt-0">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Consequuntur dicta quidem molestias harum molestiae, blanditiis
-              fuga, neque inventore repellendus consectetur, consequatur
-              corporis libero quae cumque ullam autem et aliquam odio! Lorem
-              ipsum dolor sit amet, consectetur adipisicing elit. Neque quo
-              molestiae cupiditate atque consectetur perspiciatis deserunt
-              commodi! Nesciunt, quaerat illo dolore molestiae saepe, sit
-              tempore veniam officiis odio pariatur aspernatur. Lorem ipsum
-              dolor sit, amet consectetur adipisicing elit. Illo suscipit
-              excepturi velit quaerat dolor cupiditate amet, cumque officia
-              ipsum assumenda dolores eaque quis. Exercitationem facere
-              cupiditate est corporis minus laborum?
-            </p>
-          </div>
-          <div className="w-full  border-2 border-red-500 md:m-0 md:inline-flex lg:flex-1 lg:h-fit">
+          <div className=" hidden w-full border xl:block shadow">
             <OwnerCard User={User} />
           </div>
+        </div>
+      </div>
+      <div className="my-2 space-y-3 lg:flex lg:gap-x-3 lg:space-y-0 xl:w-1/2 xl:flex-col">
+        <div className="overflow-scroll  rounded border  px-3 py-2 text-justify shadow-md md:mt-2 md:h-fit lg:mt-0 lg:flex-1 xl:h-[34.8%] xl:w-[106%]">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur dicta quidem molestias harum molestiae, blanditiis
+            fuga, neque inventore repellendus consectetur, consequatur
+            corporis libero quae cumque ullam autem et aliquam odio! Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Neque quo
+            molestiae cupiditate atque consectetur perspiciatis deserunt
+            commodi! Nesciunt, quaerat illo dolore molestiae saepe, sit
+            tempore veniam officiis odio pariatur aspernatur. Lorem ipsum
+            dolor sit, amet consectetur adipisicing elit. Illo suscipit
+            excepturi velit quaerat dolor cupiditate amet, cumque officia
+            ipsum assumenda dolores eaque quis. Exercitationem facere
+            cupiditate est corporis minus laborum?
+          </p>
+        </div>
+        <div className="w-full  border md:m-0 md:inline-flex lg:h-fit lg:flex-1 xl:hidden">
+          <OwnerCard className='' User={User} />
         </div>
       </div>
     </div>
