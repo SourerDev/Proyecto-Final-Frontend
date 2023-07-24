b28c4b635c6aa858151338e664227b973863ea09
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-07-24 20:56:09 -0300

    cleanin code

--- src/pages/profile/BePremium.jsx
+++ src/pages/profile/BePremium.jsx
@@ -24,113 +24,52 @@ export function BePremium() {
   const [linkPago, setLinkPago] = useState('#')
   const { session, signIn } = useSelector((state) => state.user)
 
-  /* if (!signIn) {
+  if (!signIn) {
     Alerts.smallWarning({ text: 'Lo sentimos, primero debes iniciar sesión' })
     navigate('/sign-in')
-  } */
+  }
   ApiPropYou.paymentUrl(session.idUser).then((res) => setLinkPago(res.data))
 
-  const _status = new URLSearchParams(location.search).get('status')
+  const status = new URLSearchParams(location.search).get('status')
   const user_id = new URLSearchParams(location.search).get('external_reference')
-  const [status, setStatus] = useState('approved')
 
-  if (_status && !signIn) {
+  if (status && !signIn) {
     // modificar despues con la tokenización y uso de cookies
-    if (_status === 'approved') {
+    if (status === 'approved') {
       ApiPropYou.setPremiumUser(user_id).then((r) => {
-        ApiPropYou.getUserById(user_id).then((res) => {
-          dispatch(setUser(res.data.user))
+        ApiPropYou.getUserById(user_id).then((r) => {
+          dispatch(setUser(r.data.user))
         })
         swal.fire(paymentOk()).then((r) => navigate('/home'))
       })
-    } else if (_status && _status === 'rejected') {
+    } else if (status && status === 'rejected') {
       ApiPropYou.getUserById(user_id).then((res) => {
         dispatch(setUser(res.data.user))
       })
-      swal.fire(paymentError()).then(r => navigate('/be-premium'))
+      swal.fire(paymentError()).then((r) => navigate('/be-premium'))
     }
-
   }
 
-  /* useEffect(() => {
-    if (status && status === 'approved') {
-      console.log(user_id)
-      ApiPropYou.setPremiumUser(session.idUser)
-    }
-    if (session?.userType === 'logged') {
-      ApiPropYou.paymentUrl(session.idUser).then((res) => setLinkPago(res.data))
-      console.log(linkPago)
-    }
-  }, []) */
-
-  const True = !signIn
-
-  if (True) return null
-
-  if (!True)
-    return (
-      <div className="flex  min-h-[600px] flex-col items-center gap-4 py-8 px-4 text-lg lg:flex-row">
-        <div className=" flex w-full flex-col gap-6 p-7 lg:w-3/6 ">
-          <h1 className="text-2xl font-semibold">Razon para ser Premium</h1>
-          <p>
-            Al volverte usuario premium pagando la membresía mensual podras
-            subir tus publicaciones para la venta o alquiler, posicionarte en el
-            mercado de propiedades, interactuar con los usuarios y comunicarte
-            con los interesados en tu publicación.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-            debitis impedit voluptatem ducimus eveniet, veritatis commodi neque
-            quos dolores corporis illum nobis, nisi laborum repellat
-            voluptatibus perferendis id delectus sapiente.
-          </p>
-        </div>
-        <div className=" grid w-3/6 place-content-center p-7">
-          <PaymentCarousel linkPago={linkPago} />
-        </div>
-      </div>
-    )
-
   return (
-    <div className="bg-[url('https://wrmx00.epimg.net/radio/imagenes/2022/02/22/martha_debayle/1645547060_000751_1645549058_noticia_normal.jpg')] bg-cover bg-center bg-no-repeat  px-3 lg:h-screen lg:px-20 ">
-      {/* <div className=" bg-opacity-75 ">
-        <div className=" flex justify-center text-center text-6xl underline ">
-          <h1 className="">Bienvenido {user?.userName} </h1>
-        </div>
-        <div className="flex   flex-col ">
-          <div className="mt-10 flex h-[10vh] flex-col items-center justify-center rounded-lg text-2xl ">
-            <div className="rounded-lg border-4 border-black bg-white bg-opacity-60 p-4 text-black shadow-2xl">
-              <h4 className="flex justify-center">Usuario PREMIUM</h4>
-              <p>Costo mensual: $5.000 (ars)</p>
-            </div>
-          </div>
-        </div>
-        <div className="bg mb-3 mt-20 flex justify-center rounded-lg bg-white  bg-opacity-60  p-10 px-10  lg:mb-20 ">
-          <p className="text-2xl text-black "></p>
-        </div>
-
-        <div className="mb-20 flex justify-center">
-          {linkPago !== '#' ? (
-            <a
-              className=" whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-8 py-4 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-              href={linkPago}
-              onClick={(e) => setRedirect(true)}
-              target=""
-            >
-              Hacerme premium!
-            </a>
-          ) : (
-            <div className="flex justify-center ">
-              <button
-                className="w-50  m-3 mx-auto flex rounded border-2 border-blue-600 p-6 px-8 py-4 text-xs font-medium uppercase leading-normal text-blue-600 transition duration-150 ease-in-out focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:border-gray-800 disabled:bg-gray-300 disabled:text-gray-700 hover:bg-black hover:bg-opacity-5"
-                disabled={true}
-              >
-                Hacerme premium!
-              </button>
-            </div>
-          )}
-        </div>
-      </div> */}
+    <div className="flex  min-h-[600px] flex-col items-center gap-4 py-8 px-4 text-lg lg:flex-row">
+      <div className=" flex w-full flex-col gap-6 p-7 lg:w-3/6 ">
+        <h1 className="text-2xl font-semibold">Razon para ser Premium</h1>
+        <p>
+          Al volverte usuario premium pagando la membresía mensual podras subir
+          tus publicaciones para la venta o alquiler, posicionarte en el mercado
+          de propiedades, interactuar con los usuarios y comunicarte con los
+          interesados en tu publicación.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni debitis
+          impedit voluptatem ducimus eveniet, veritatis commodi neque quos
+          dolores corporis illum nobis, nisi laborum repellat voluptatibus
+          perferendis id delectus sapiente.
+        </p>
+      </div>
+      <div className=" grid w-3/6 place-content-center p-7">
+        <PaymentCarousel linkPago={linkPago} />
+      </div>
     </div>
   )
 }
